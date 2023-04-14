e66d2fd72b3518a69df50d48a8bf7a9003af704f
Gunangad Pal Singh Narula <50276105+angad11121@users.noreply.github.com>
2023-04-15 04:13:47 +0530

    Add switchable login/logout (WIP)

--- frontend/src/components/LoginButton.js
+++ frontend/src/components/LoginButton.js
@@ -1,32 +1,45 @@
-import React, { useState } from "react";
-// import { Button, Divider} from "@mantine/core";
-import { GoogleLogin } from "@react-oauth/google";
+import React, { useState ,useEffect} from "react";
+import axios from 'axios';
+import {Text, Button} from "@mantine/core";
+import { useGoogleLogin, googleLogout } from "@react-oauth/google";
 
 function LoginButton() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState([]);
+  const [ profile, setProfile ] = useState([]);
 
-  const login = (
-    <GoogleLogin
-      onSuccess={(credentialResponse) => {
-        console.log(credentialResponse);
-        setLoggedIn(true);
-      }}
-      onError={() => {
-        console.log("Login Failed");
-      }}
-    />
+  const loginFunction = useGoogleLogin({
+    onSuccess:(credentialResponse) => {
+      console.log(credentialResponse);
+      setUser(credentialResponse)
+    },
+    onError:() => {
+      console.log("Login Failed");
+    }});
+  
+    useEffect(
+      () => {
+          if (user) {
+              axios
+                  .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                      headers: {
+                          Authorization: `Bearer ${user.access_token}`,
+                          Accept: 'application/json'
+                      }
+                  })
+                  .then((res) => {
+                      setProfile(res.data);
+                  })
+                  .catch((err) => console.log(err));
+          }
+      },
+      [ user ]
   );
 
-  //   const logout = (<googleLogout
-  //   onLogoutSuccess={()=>{
-  //     console.log('Logout Success');
-  //     setLoggedIn(false)
-  // }}
-  // onError={()=>{
-  //   console.log('Logout Failed')
-  // }}
-  // />)
-  return <div>{loggedIn ? login : login}</div>;
+
+
+  const login = (<Button onClick={()=>loginFunction()}>Sign in with Google</Button> )
+  const logout = (<Button onClick={()=>{ googleLogout(); setProfile(null);}}>Sign Out</Button>)
+  return <div>{profile? <div> <Text>Hi {profile.name}, {user.keys}</Text> {logout} </div> : login}</div>;
 }
 
 export default LoginButton;
