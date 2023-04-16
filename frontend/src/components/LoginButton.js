4ecc8a88d131c474be87c30695de3607f54758dd
Gunangad Pal Singh Narula <50276105+angad11121@users.noreply.github.com>
2023-04-17 00:39:07 +0530

    Fix Login Session

--- frontend/src/components/LoginButton.js
+++ frontend/src/components/LoginButton.js
@@ -1,47 +1,63 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { Text, Button } from "@mantine/core";
 import { useGoogleLogin, googleLogout } from "@react-oauth/google";
 import SearchBox from "./SearchBox";
 import UploadButton from "./UploadButton";
 
 function LoginButton() {
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState(null);
   const [profile, setProfile] = useState(null);
 
   const loginFunction = useGoogleLogin({
     onSuccess: (credentialResponse) => {
-      console.log(credentialResponse);
-      setUser(credentialResponse)
+      localStorage.setItem("user", JSON.stringify(credentialResponse));
+      setUser(credentialResponse);
     },
     onError: () => {
       console.log("Login Failed");
-    }
+    },
   });
 
-  useEffect(
-    () => {
-      if (user) {
-        axios
-          .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+  const logoutFunction = () => {
+    googleLogout();
+    localStorage.setItem("user", JSON.stringify(null));
+    setProfile(null);
+  };
+
+  useEffect(() => setUser(JSON.parse(localStorage.getItem("user"))), []);
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
             headers: {
               Authorization: `Bearer ${user.access_token}`,
-              Accept: 'application/json'
-            }
-          })
-          .then((res) => {
-            setProfile(res.data);
-          })
-          .catch((err) => console.log(err));
-      }
-    },
-    [user]
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const login = (
+    <Button onClick={() => loginFunction()}>Sign in with Google</Button>
   );
-
-
-
-  const login = (<Button onClick={() => loginFunction()}>Sign in with Google</Button>)
-  const logout = (<Button onClick={() => { googleLogout(); setProfile(null); }}>Sign Out</Button>)
+  const logout = (
+    <Button
+      onClick={() => {
+        logoutFunction();
+      }}
+    >
+      Sign Out
+    </Button>
+  );
 
   // Conditional rendering based on profile state
   return (
