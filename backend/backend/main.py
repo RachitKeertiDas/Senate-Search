25f50cd060b9c71d3312f6c80f4bb368fc7df6cd
Rachit Keerti Das <rachitkdas@gmail.com>
2023-04-17 16:11:04 +0530

    Init Upload file Integration
    Move extract.py to the same folder. Will be separated into modules later

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,10 +1,17 @@
 from typing import Union
 
 from fastapi import Depends, FastAPI, Header, HTTPException, Response
+from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Header, HTTPException, Response, File, UploadFile
 from dotenv import load_dotenv
 
 from auth import get_user_email
+from extract import SenateMinutes
 app = FastAPI()
+
+def extract_file(filename):
+    print(filename)
+    extracted_cnt = SenateMinutes(filename, senate_number=49)
 
 def verify_auth_token(Authorization: str = Header()):
     email = get_user_email(Authorization)
@@ -25,4 +32,22 @@ async def auth(email: str = Depends(verify_auth_token)):
     Test Endpoint to validate user identity.
     """
     return {"email": email}
+
+@app.post("/uploadfile")
+async def upload_file(file: UploadFile):
+    """
+    Upload Documents, To be Protected by Auth and Accessible to Admin Users only
+    """
+    print(file)
+    file_cnt = file.file.read()
+    try:
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"msg":"Saving the File Failed"}
+    finally:
+        file.file.close()
+        extract_file(file.filename)
+
+    return {file.filename}
 
