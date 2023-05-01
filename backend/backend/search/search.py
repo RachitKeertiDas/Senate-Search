9d190a714984061e66cfa0cf08dfeaecabb3c7a4
dark-trojan <karthikbalaji01@gmail.com>
2023-05-02 04:28:12 +0530

    Refactor search functionality
    Object oriented design for a search engine class

--- backend/backend/search/search.py
+++ backend/backend/search/search.py
@@ -1,51 +1,49 @@
+from __future__ import annotations
 import numpy as np
 from sentence_transformers import SentenceTransformer
 import faiss
 import time
-from extract import SenateMinutes
+
+from extract.extract import SenateMinutes
 
 
-def fetch_minutes_info(minutes, resolution_idx):
-    return minutes.proposals[resolution_idx]
+class SearchEngine:
+    def __init__(self):
+        self.model = SentenceTransformer('msmarco-distilbert-base-dot-prod-v3')
 
+    def _fetch_minutes_info(self, minutes, doc_index, resolution_idx):
+        return minutes[doc_index[resolution_idx][0]].proposals[doc_index[resolution_idx][1]]
 
-def search(query, minutes, top_k, index, model):
-    t = time.time()
-    query_vector = model.encode([query])
-    top_k_results = index.search(query_vector, top_k)
-    print('Top-K:', top_k_results)
-    print('>>>> Results in Total Time: {}'.format(time.time()-t))
-    top_k_ids = top_k_results[1].tolist()[0]
-    top_k_ids = list(dict.fromkeys(top_k_ids))
-    results = [fetch_minutes_info(minutes, idx) for idx in top_k_ids]
-    return results
+    def _faiss_search(self, query, minutes, doc_index, top_k, index):
+        t = time.time()
+        query_vector = self.model.encode([query])
+        top_k_results = index.search(query_vector, top_k)
+        # print('>>>> Results in Total Time: {}'.format(time.time()-t))
+        top_k_ids = top_k_results[1].tolist()[0]
+        top_k_ids = list(dict.fromkeys(top_k_ids))
+        results = [self._fetch_minutes_info(
+            minutes, doc_index, idx) for idx in top_k_ids]
+        return results
 
+    def search(self, query, minutes: list[SenateMinutes]):
+        docs, doc_index = [], []
+        for m, minute in enumerate(minutes):
+            for i in range(len(minute.proposals)):
+                doc = minute.proposals[i]['proposal_id'] + ' ' + \
+                    minute.proposals[i]['proposal'] + '\n' + \
+                    minute.proposals[i]['resolution']
+                docs.append(doc)
+                doc_index.append((m, i))
 
-filepath = '../../../Minutes/51st Senate/51st Senate minutes.pdf'
-minutes = SenateMinutes(filepath=filepath, senate_number=51)
-minutes.extract()
-docs = []
-for i in range(len(minutes.proposals)):
-    doc = minutes.proposals[i]['proposal_id'] + ' ' + \
-        minutes.proposals[i]['proposal'] + '\n' + \
-        minutes.proposals[i]['resolution']
-    docs.append(doc)
+        # Process data
+        encoded_data = self.model.encode(docs)
+        encoded_data = np.asarray(encoded_data.astype('float32'))
+        index = faiss.IndexIDMap(faiss.IndexFlatIP(768))
+        index.add_with_ids(encoded_data, np.array(range(0, len(docs))))
+        faiss.write_index(index, 'senate_resolution.index')
 
-# Model
-model = SentenceTransformer('msmarco-distilbert-base-dot-prod-v3')
+        # Search within the documents
+        results = self._faiss_search(query, minutes, doc_index,
+                                     top_k=10, index=index)
 
-# Process data
-encoded_data = model.encode(docs)
-encoded_data = np.asarray(encoded_data.astype('float32'))
-index = faiss.IndexIDMap(faiss.IndexFlatIP(768))
-index.add_with_ids(encoded_data, np.array(range(0, len(docs))))
-faiss.write_index(index, 'senate_resolution.index')
-
-
-# Query
-query = "Additional Course Conversion"
-results = search(query, minutes, top_k=10, index=index, model=model)
-print('Query:', query)
-print("\n")
-for result in results:
-    print('\t', result)
+        return results
