8605516c908de21a200d59a7fb1b3e810a7cb380
Rachit Keerti Das <rachitkdas@gmail.com>
2023-05-02 05:46:11 +0530

    Fix Handbook route

--- frontend/src/Pages/Handbook.js
+++ frontend/src/Pages/Handbook.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Center, Card, Text, Container } from "@mantine/core";
-import { Viewer, Worker } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import Shell from "../components/AppShell";
 
 function Handbook() {
@@ -11,60 +11,51 @@ function Handbook() {
   const [pdfFile, setPdfFile] = useState(null);
 
   useEffect(() => {
-    axios.get("https://example.com/path/to/pdf/file.pdf", {
-      responseType: "arraybuffer",
-    })
-    .then(response => {
-      const pdfBlob = new Blob([response.data], { type: "application/pdf" });
-      const pdfUrl = URL.createObjectURL(pdfBlob);
-      setPdfFile(pdfUrl);
-    })
-    .catch(error => {
-      console.error(error);
-    });
+    axios
+      .get("http://localhost:8000/handbook", {
+        responseType: "arraybuffer",
+      })
+      .then((response) => {
+        const pdfBlob = new Blob([response.data], { type: "application/pdf" });
+        const pdfUrl = URL.createObjectURL(pdfBlob);
+        setPdfFile(pdfUrl);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
 
   return (
     <Shell
       content={
-        <div>
-          <Center
-            style={{
-              height: "50vh",
-              maxWidth: "100vw",
-              backgroundColor: "#FFE8CC",
-            }}
-          >
-            <Card>
-              <Container>
-                <Text fw={700}>
-                  Welcome to IITH Senate Search portal- Handbook.
-                  <br />
-                </Text>
-                <Text>
-                  Here you can easily view and track previous Senate Meeting
-                  Minutes.
-                </Text>
-              </Container>
-            </Card>
-          </Center>
+        <Center
+          style={{
+            height: "85vh",
+            maxWidth: "100vw",
+            backgroundColor: "#FFE8CC",
+          }}
+        >
           {pdfFile && (
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.5.141/build/pdf.worker.min.js">
-              <Viewer fileUrl={pdfFile} plugins={[defaultLayoutPluginInstance]} />
+              <Viewer
+                fileUrl={pdfFile}
+                plugins={[defaultLayoutPluginInstance]}
+              />
             </Worker>
           )}
-          {!pdfFile && 
+          {!pdfFile && (
             <Center>
               <Text
                 style={{
                   color: "red",
                   fontSize: "50px",
                 }}
-              >No PDF Found!!!
+              >
+                No PDF Found!!!
               </Text>
             </Center>
-          }
-        </div>
+          )}
+        </Center>
       }
     />
   );
